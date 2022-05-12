232ca1ef4fdbb4c4f495ef784fe2f962dd149d69
Shah Hiral <73848946+7984495444@users.noreply.github.com>
2022-05-12 14:36:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = var.aws_region
+  access_key = "AKIAS62M2BSUXGMHHXUD"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
